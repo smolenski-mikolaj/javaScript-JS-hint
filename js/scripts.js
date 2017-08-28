f1875ad0c7d3ab822dbba415c1edb2c830e99fce
Mikołaj Smoleński <smolenski.mikolaj@gmail.com>
2017-08-28 21:34:15 +0200

    update JS-hint

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var a = 3,
+	b = 2,
+	value;
+	
+	value = (a * a) + (2 * a * b) - (b * b);
+	console.log('Wynik: ' + value);
+	
+if ( isNaN(a) == false &&  isNaN(b) == false ) {	
+	if (value > 0) {
+		console.log('Wynik dodatni');
+	} else if (value < 0) {
+		console.log('Wynik ujemny');
+	} else if (value == 0) {
+		console.log('Wynik równy 0');
+	}
+}
